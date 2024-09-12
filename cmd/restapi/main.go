ad97870a8b82eeb75bc5e82dd9f6906be171c94e
Miron <miron.nicolaevich@gmail.com>
2024-09-12 16:09:15 +0200

    feat(study): create routes

--- cmd/restapi/main.go
+++ cmd/restapi/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log"
 	"net/http"
-	"time"
-	"log/slog"
 )
 
-func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(1000 * time.Millisecond))
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET http://127.0.0.1:8080/")
+	w.Write([]byte("Hello world"))
 
-	defer cancel()
-	slog.
-
-	
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET http://127.0.0.1:8080/login")
+	w.Write([]byte("Welcome"))
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET http://127.0.0.1:8080/logout")
+	w.Write([]byte("Goodbay"))
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	log.Println("Run server http://127.0.0.1:8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+
+}
