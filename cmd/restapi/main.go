bdc06101182d6b4584f7063bbab05c22af0113cc
Miron <miron.nicolaevich@gmail.com>
2024-09-12 17:57:51 +0200

    feat(test): add GET/notes&id=<int>

--- cmd/restapi/main.go
+++ cmd/restapi/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -18,20 +20,27 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	// вход
+	// Полуваем все заметки
 	log.Println("GET status 200 http://127.0.0.1:8080/login")
 	w.Write([]byte("Welcome"))
 
 }
 
-func logout(w http.ResponseWriter, r *http.Request) {
-	// выход из учетки
-	log.Println("GET status 200 http://127.0.0.1:8080/logout")
-	w.Write([]byte("Goodbay"))
+func get_notes_by_id(w http.ResponseWriter, r *http.Request) {
+	// Получаем заметку по ID
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		log.Printf("GET status 400 http://127.0.0.1:8080/ params: %v.", id)
+		http.NotFound(w, r)
+		return
+	}
+
+	log.Printf("GET status 200 http://127.0.0.1:8080/notes?id=%d", id)
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 
 }
 
-func notes(w http.ResponseWriter, r *http.Request) {
+func post_new_note(w http.ResponseWriter, r *http.Request) {
 	// Создает новую заметку
 	if r.Method != http.MethodPost {
 		log.Println("POST status 405 http://127.0.0.1:8080/notes")
@@ -41,7 +50,7 @@ func notes(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json") // сначало нужно установить заголовокБ а потом статус-код
-	w.WriteHeader(201) //  вызывать можно только один раз перед отправкой ответа.
+	w.WriteHeader(201)                                 //  вызывать можно только один раз перед отправкой ответа.
 	w.Write([]byte(`{"status": OK}`))
 
 }
@@ -50,8 +59,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("/notes", notes)
+	mux.HandleFunc("/notes", get_notes_by_id)
+	mux.HandleFunc("/notes/new", post_new_note)
 	log.Println("Run server http://127.0.0.1:8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
